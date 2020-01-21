86d891b1de95ae296623ccfeff1694e86c90ef45
MrDrummer25 <mrdrummer.programming@hotmail.com>
2020-01-21 18:24:40 +0000

    typescript to javascript

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import * as fs from "fs"
-import luamin from "luamin"
+const fs = require("fs")
+const luamin = require("luamin")
 
 const files = fs.readdirSync("./src/")
 if (files.length > 0) {
-  console.log("SUCCESS!!", files)
   for (const file of files) {
     const fileContents = fs.readFileSync("./src/" + file, "utf8")
     // console.log("fileContents :", fileContents)
